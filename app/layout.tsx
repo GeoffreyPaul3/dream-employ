0ec74c6a5f4ba6a223fbcd7ceb6957c0616ab9bc
GeoffreyPaul3 <110961738+Gtigercode@users.noreply.github.com>
2024-05-31 13:50:55 +0200

    Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -23,7 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.variable}>{children}</body>
+      <body className={poppins.variable}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
